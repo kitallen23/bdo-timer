9c028e94f2c5c8687bcbcc80e5bc6b7501f58834
Kit Allen <kitallen23@gmail.com>
2017-09-22 18:45:26 +1000

    Added 404 page

---
+++ resources/views/error/missing.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.master')
+@section('title', '404')
+@section('content')
+    <!-- Scripts etc. -->
+
+    <div class="col-xs-12 timerbar-wrapper">
+        <div class="progress col-xs-12 progress-custom-lg" id="timerbar-default" style="display:none;"></div>
+    </div>
+
+    <div class="container">
+
+        <div class="content">
+
+
+            <div class="col-xs-8 col-xs-offset-2 text-center" style="margin-top:200px;">
+
+                <h3>This PAGE is not in place currently. Please come back between 5 AM and 7 PM.</h3>
+                <img src="{{URL::asset('img/err/404.png')}}" style="margin-top:40px;" width="350" />
+
+            </div>
+
+            <!-- VIEW SESSION -->
+            {{--<div class="col-xs-12 session-data">{{ var_dump(Session::get('items')) }}</div>--}}
+
+        </div>
+    </div>
+@endsection
